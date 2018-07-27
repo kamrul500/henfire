ec797f39f3a66902897a75607a6cf44e439ff518
Matthuffy <adtelly71@gmail.com>
2018-07-27 15:15:32 +0200

    Add files via upload

---
+++ protected/backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 't4-JmOd6GQg7m5vvvrWNSj8veWJRQtiX',
+        ],
+    ],
+];
